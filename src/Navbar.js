0e4c106c26f9c17d350fab856f5b70dc5c4920f2
UdhayaKumar <udhayakumar2352000@gmail.com>
2023-05-28 21:48:52 +0530

    name

--- src/Navbar.js
+++ src/Navbar.js
@@ -22,7 +22,7 @@ const Navbar = () => {
     <nav>
       <div className='nav-center'>
         <div className='nav-header'>
-          <img  className='logo' alt='ZORO' />
+          <img  className='logo' alt='ZOROUDHAYA' />
           <button className='nav-toggle' onClick={toggleLinks}>
             <FaBars />
           </button>
